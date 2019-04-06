361db2132ee53a023f84045ef29299f02da3ec4f
arsonite <notarson@gmail.com>
2019-04-06 15:07:20 +0200

    finished compression algorithm

--- lessify.sh
+++ lessify.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
-# Implement algorith which flags already compressed and compiled files with this shell script with a /*#!c!#*/ flag to save processing power by skipping them
+# Function that appends a custom flag to the beginning of the specified file
+flagFile() {
+	flag=$1
+	file=$2	
+	# Print the flag and append it to the beginning of the temporary through the pipeline
+	echo $flag | cat - $file > temp && mv temp $file
+}
 
+# Function that compiles and compresses .less files
 compileAndCompress() {
-	v=$1
+	v=([ "$1" = "-v" ]) # Conditional statement that determines if verbose flag is set on script
+	flag="/*#C#*/" # Custom appendage flag
 	
-	[ $v = 1 ] && echo ... Processing "$line"
+	$v && echo ... Processing "$line"
 	replace="css"
-	css=${line//less/$replace}
-	[ $v = 1 ] && echo ... Compiling "$line"
-	lessc $line $css
-	[ $v = 1 ] && echo ... Finished compiling "$line"
+	css=${line//less/$replace} # Parse and replace the path of the .less file with the new .css file
+		
+	if [ -a $css ]; then
+		content=`cat $css`
+		if [[ $content == *$flag* ]]; then # Reads file and evaluates if compression flag was set
+			$v && echo "File exists and is already flagged. Aborting..."
+			return
+		fi
+	fi
 
-	[ $v = 1 ] && echo ... Applying compression on "$css"
-	cleancss -o $css $css
-	[ $v = 1 ] && echo ... Finished compressing "$css"
-	[ $v = 1 ] && echo Saved, the compiled and compressed .css under "$css"
+	$v && echo ... Compiling "$line"
+	lessc $line $css # Compile the given .less file with the lessc-cli
+	$v && echo ... Finished compiling "$line"
+
+	$v && echo ... Applying compression on "$css"
+	cleancss -o $css $css # Compress the .css file with the cleancss-cli
+	$v && echo ... Finished compressing "$css"
+	$v && echo Saved, the compiled and compressed .css under "$css"
+
+	flagFile $flag $css
+	$v && echo "... Applied flag " $flag " to " $css
 }
 
 find . -name "*.less" | while read line; do
-	if [ "$1" = "-v" ]; then
-		compileAndCompress 1
-	else
-		compileAndCompress 0
-	fi
+	compileAndCompress $1
 done
 
 echo Exiting script.
