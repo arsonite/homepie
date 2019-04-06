77610c11ccd6cc78ad22b360becd3dc642a58599
arsonite <notarson@gmail.com>
2019-04-06 15:42:58 +0200

    added additional comments

--- py/caching.py
+++ py/caching.py
@@ -1,13 +1,24 @@
-#!/usr/bin/env
+#!/usr/bin/env/python
 import sys;
 import os;
 import shutil;
+import socket;
+import re;
 
-# CONST-definitions
-ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print(ROOT);
+# Get hostname of current machine
+HOST = socket.gethostname()
+# Backwards iterative directory change algorithm to get root, dependent on current hostname
+inHostRoot = False
+root = os.path.abspath(__file__)
+# Regex that matches the hostname being the last directory as root
+regex = "^\/.*(" + HOST.replace(".", "\.") + ")$"
+while inHostRoot == False:
+	root = os.path.dirname(root)
+	# RegEx to determine if current directory is root directory
+	if re.search(regex, root):
+		inHostRoot = True
 
-# Outsource to JSON shared between JS, PHP and python
+# TO-DO: Outsource to JSON shared between JS, PHP and python
 SUB_DIRS = ["img", "sfx", "txt", "vid"]
 
 PERM_OPT = ["1", "2", "3", "4", "5"]
@@ -53,7 +64,6 @@ def moveContent():
 def createCache():
 	foo = input('Which cache do you want to create?')
 
-# Operations
 print('Python%d.%d: Script started...\n' % (sys.version_info[0], sys.version_info[1]))
 
 userInput = ""
