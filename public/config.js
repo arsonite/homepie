a118abf78c87c8ecd07f821468fae0c462df2e77
arsonite <notarson@gmail.com>
2019-04-11 18:42:53 +0200

    styling changes

--- public/config.js
+++ public/config.js
@@ -1,3 +1,10 @@
 export default const config = {
-	"API_URL": "api.homepie.net"
+	"API_URL": "api.homepie.net",
+
+	"GET_FILES": "",
+	"POST_FILES": "",
+	"GET_SALT": "",
+	"GET_PRIVATE_KEY": "",
+
+	"NAV": ["", "", "", "", "", ""]
 }
