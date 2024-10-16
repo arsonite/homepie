2e83e9e2e41a96227b03590390e56015be3124f0
arsonite <tr_burak@hotmail.de>
2024-10-16 21:56:38 +0000

    Refactor file paths and update copyright notice

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,53 @@
+/**
+ * @license
+ * Copyright (c) 2024-2024 Burak Günaydin
+ * All Rights Reserved
+ *
+ * This software is the confidential and proprietary information of
+ * Burak Günaydin. You may not use, modify, or distribute this
+ * software (unless you have the permission of the copyright holder)
+ * except in accordance with the terms of any applicable license agreement.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALI* NGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * ESLint configuration file for the project.
+ *
+ * @file .eslintrc.cjs
+ * @description This configuration sets up ESLint for a TypeScript and React project.
+ * It includes recommended rules for ESLint, TypeScript, and React Hooks, and it uses
+ * the `@typescript-eslint/parser` to parse TypeScript code.
+ *
+ * @property {boolean} root - Indicates that this is the root configuration file.
+ * @property {Object} env - Specifies the environments for the code.
+ * @property {boolean} env.browser - Enables browser global variables.
+ * @property {boolean} env.es2020 - Enables ES2020 syntax.
+ * @property {string[]} extends - Extends recommended ESLint, TypeScript, and React Hooks rules.
+ * @property {string[]} ignorePatterns - Specifies files and directories to ignore.
+ * @property {string} parser - Specifies the parser to use for TypeScript.
+ * @property {string[]} plugins - Specifies additional plugins to use.
+ * @property {Object} rules - Custom rules configuration.
+ * @property {Array} rules['react-refresh/only-export-components'] - Warns if components are not exported correctly, allows constant exports.
+ * @property {string} rules['no-unused-vars'] - Disables the default ESLint rule for unused variables.
+ * @property {string} rules['@typescript-eslint/no-unused-vars'] - Warns about unused variables in TypeScript.
+ */
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+    },
+};
---
+++ client/vite.config.ts
@@ -0,0 +1,102 @@
+/**
+ * @license
+ * Copyright (c) 2024-2024 Burak Günaydin
+ * All Rights Reserved
+ * 
+ * This software is the confidential and proprietary information of
+ * Burak Günaydin. You may not use, modify, or distribute this
+ * software (unless you have the permission of the copyright holder)
+ * except in accordance with the terms of any applicable license agreement.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALI* NGS IN THE
+ * SOFTWARE.
+ */
+
+// Package imports
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import react from '@vitejs/plugin-react';
+
+// Read environment variables
+const VITE_SSL_CERT_PATH = process.env.VITE_SSL_CERT_PATH;
+const VITE_SSL_KEY_PATH = process.env.VITE_SSL_KEY_PATH;
+
+// https://vitejs.dev/config/
+/**
+ * Vite configuration file for the HomePie client application.
+ * 
+ * This configuration includes settings for:
+ * - Global SCSS constants
+ * - Workaround for missing global variable in simple-peer
+ * - Plugin configuration
+ * - Path aliasing for nested components
+ * - HTTPS development server settings
+ */
+
+export default defineConfig({
+  /**
+   * CSS preprocessor options.
+   * 
+   * Enables global usage of SCSS constants by importing global mixins and variables.
+   */
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/_style/global-mixins.scss";
+          @import "./src/_style/global-variables.scss";
+        `,
+      },
+    },
+  },
+
+  /**
+   * Define global variables.
+   * 
+   * Workaround for simple-peer missing global variable.
+   */
+  define: {
+    global: {}
+  },
+
+  /**
+   * Plugin configuration.
+   * 
+   * Not using mkcert() anymore, because it overwrites the SSL certificate served by the development server.
+   */
+  plugins: [react()],
+
+  /**
+   * Module resolution configuration.
+   * 
+   * Allows nested components in source code to resolve the absolute path via "@".
+   */
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+
+  /**
+   * HTTPS development server settings.
+   * 
+   * - Enables CORS.
+   * - Sets the host to 'localhost' to circumvent insecure SSL certificate warnings in browsers.
+   * - Configures HTTPS with specified certificate and key files.
+   * - Automatically opens the browser on server start.
+   * - Sets the server port to 443.
+   */
+  server: {
+    cors: true,
+    host: '0.0.0.0', // Circumvents the insecure-ssl-certificate-warning in browsers
+    https: {
+      cert: fs.readFileSync(VITE_SSL_CERT_PATH),
+      key: fs.readFileSync(VITE_SSL_KEY_PATH)
+    },
+    open: false, // Set this to false to prevent opening a browser
+    port: 443
+  }
+});
