4efda5b805e73f2aa4244b84e89c393f6892d38e
arsonite <tr_burak@hotmail.de>
2024-10-19 16:21:06 +0200

    Add CustomHelpFormatter class for customizing argument help formatting

---
+++ cli/src/CustomHelpFormatter.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2024-2024 Burak Günaydin
+# All Rights Reserved
+#
+# This software is the confidential and proprietary information of
+# Burak Günaydin. You may not use, modify, or distribute this
+# software (unless you have the permission of the copyright holder)
+# except in accordance with the terms of any applicable license agreement.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# System imports
+import argparse
+
+class CustomHelpFormatter(argparse.HelpFormatter):
+    def _format_args(self, action, default_metavar=None):
+        # Handle required arguments
+        if action.required:
+            return f'<{action.dest}>'
+        # Handle optional arguments
+        return super()._format_args(action, default_metavar)
